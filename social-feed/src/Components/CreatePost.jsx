3ca704711bbb81e7f4a61c2d25ad2de35066da64
Karim <aarrale12@gmail.com>
2022-08-07 11:37:29 -0500

    Create Post component

--- social-feed/src/Components/CreatePost.jsx
+++ social-feed/src/Components/CreatePost.jsx
@@ -1,5 +1,48 @@
-import "./CreatePost.css"
+import { useState } from "react"
 
 const CreatePost = (props) => {
-    const
+    const [name, setName] = useState('');
+    cost [post, setPost] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        
+        let newPost = {
+            name: name,
+            post: post
+        };
+
+        if(newPost.name === ""|| newPost.post === ""){
+            alert("One or more fields required")
+        }
+        else{
+            
+            props.addNewPost(newPost)
+        }
+        setName('');
+        setPost('');
+    }
+
+    return ( 
+        <form class = "form"id="form" onSubmit={handleSubmit} >
+            <div class="input">
+                <div>
+                    <label>Name</label>
+                    <input class = "input-name" id ="name"type='text' value={name} onChange={(event)=> setName(event.target.value)}/>
+                </div>
+                <div class = "bottom-half">
+                    <label>Post</label>
+                    <div class="bottom-right">
+                        <textarea class="input-post" id = 'post'type="text" value={post} onChange={(event)=> setPost(event.target.value)} />
+                        <button class="create-button" type="submit">Create</button>
+                    </div>
+                    </div>
+                    
+            </div>
+           
+            
+        </form>
+     );
 }
+ 
+export default CreatePost;
