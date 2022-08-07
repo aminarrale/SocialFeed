2e17d44ef0bd365821edce20906bd38338094f5b
Karim <aarrale12@gmail.com>
2022-08-07 12:26:01 -0500

    Navbar added

---
+++ social-feed/src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = (props) => {
+    return (
+        <header class="nav-header">
+            <div class="nav-div">
+                <p>Social</p><p class="feed">Feed</p>
+            </div>
+        </header>
+    );
+}
+export default NavBar;
